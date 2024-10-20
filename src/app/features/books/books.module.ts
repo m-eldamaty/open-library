3cbd1e87ad7b98feba92eb7db9249b740af6569c
mayar-samy <mayar.samy@akqa.com>
2024-10-20 15:55:27 +0300

    feat: Add loading indicator

--- src/app/features/books/books.module.ts
+++ src/app/features/books/books.module.ts
@@ -1,21 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { BookDetailsComponent } from './book-details/book-details.component';
-import { AuthorDetailsComponent } from './author-details/author-details.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { BookDetailsComponent } from "./book-details/book-details.component";
+import { AuthorDetailsComponent } from "./author-details/author-details.component";
+import { LoadingComponent } from "src/app/shared/loading/loading.component";
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    BookDetailsComponent,
-    AuthorDetailsComponent
-  ],
-  imports: [
-    CommonModule,
-	RouterModule
-  ]
+	declarations: [
+		HomeComponent,
+		BookDetailsComponent,
+		AuthorDetailsComponent,
+		LoadingComponent,
+	],
+	imports: [CommonModule, RouterModule],
 })
-export class BooksModule { }
+export class BooksModule {}
--- src/app/features/books/home/home.component.ts
+++ src/app/features/books/home/home.component.ts
@@ -1,28 +1,33 @@
-import { Component } from '@angular/core';
-import { Book } from '../mock-data/book.model';
-import { BOOKS } from '../mock-data/mock-books';
+import { Component } from "@angular/core";
+import { Book } from "../mock-data/book.model";
+import { BOOKS } from "../mock-data/mock-books";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: "app-home",
+	templateUrl: "./home.component.html",
+	styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent {
-	books: Book[] = []; 
+	books: Book[] = [];
+	loading: boolean = true;
 
 	ngOnInit(): void {
-	  this.loadFinanceBooks();
+		this.loadFinanceBooks();
 	}
-  
+
 	loadFinanceBooks(): void {
-	  this.books = BOOKS.filter(book => book.subjects.includes('Finance')).slice(0, 9);
+		setTimeout(() => {
+			this.loading = false;
+		}, 2000);
+		this.books = BOOKS.filter((book) =>
+			book.subjects.includes("Finance")
+		).slice(0, 9);
 	}
 	getBookCoverUrl(book: Book): string {
 		if (book.cover_id) {
-		  return `assets/images/${book.cover_id}.jpg`;
+			return `assets/images/${book.cover_id}.jpg`;
 		} else {
-		  return 'assets/images/default-cover.png';
+			return "assets/images/default-cover.png";
 		}
-	  }
-	  
+	}
 }
